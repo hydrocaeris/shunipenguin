0dce5ffa59381e11b8f953fdfa7ab3d41eedce25
hydrocaeris <nurfatihsatria@gmail.com>
2025-03-07 21:45:28 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import ReactDOM from "react-dom/client";
 
 function App() {
+ 
+function Total() {
+  // Deklarasi state arr sebagai array dengan nilai awal [1]
+  const [arr, setArr] = useState([1]); // deklarasi dan inisialisasi, local scope
+
+  function handleClick() {
+    // Menambahkan nilai 1 ke array dengan menggunakan spread operator
+    setArr([...arr, 1]); // memperbarui state arr dengan menambahkan 1 di akhir array
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Total Length Array: {arr.length}</p>
+      <button onClick={handleClick}>Tambah</button>
     </div>
   );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Total />);
+
 }
 
 export default App;
